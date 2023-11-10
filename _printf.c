8cc068a743e1da7757aae3d071e9f674781f20e5
Mvoii <mvoifranklin@gmail.com>
2023-11-10 04:16:00 +0300

    initial commits

---
+++ _printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * _prntf - produces output according to a format
+ * 
+ * @format: a character string
+ * 
+ * Return: The number of characters tat were printed
+*/
+int _printf(const char *format, ...)
